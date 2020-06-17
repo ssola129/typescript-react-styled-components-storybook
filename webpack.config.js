f0dc39aa291625a19eeb97f0d4be63bb43cba135
ssola129 <ssola129@gmail.com>
2020-06-17 18:16:32 +0900

    webpack 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components')
+  .default;
 const styledComponentsTransformer = createStyledComponentsTransformer();
 
 module.exports = {
@@ -17,14 +18,32 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      loader: 'ts-loader',
-      options: {
-        getCustomTransformers: () => ({
-          before: [styledComponentsTransformer],
-        }),
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          getCustomTransformers: () => ({
+            before: [styledComponentsTransformer],
+          }),
+        },
       },
-    }],
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'url-loader',
+        include: [/assets/],
+        options: {
+          limit: 10000,
+        },
+      },
+      {
+        test: /\.(ttf|eot|otf|woff2?)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        include: [/assets/],
+        query: {
+          name: '[name].[ext]',
+        },
+      },
+    ],
   },
 };
